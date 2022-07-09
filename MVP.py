c3cbbf751fd0536c11ec134a553a4da83d76885f
Petr <ppritup2002@mail.ru>
2022-07-09 16:49:03 +0300

    feat(gh-1): add filetype + config usage

--- MVP.py
+++ MVP.py
@@ -1,25 +1,25 @@
+import argparse
+import json
+import magic
 import requests
 import response 
-import argparse
-OAuth = 'AQAAAAAudcyyAAgGh2GQf9VpU0cPoump_hoWhcI'
-YandexDownloadUrl = "https://cloud-api.yandex.net/v1/disk/resources/upload"
-headers = {'Accept' : 'application/json', 'Authorization' : OAuth}
-# file_path = './studentID.jpg'
-Content_type = "image/jpeg"
-# filename = "studentID.jpg"
+
 
 def GetDownloadUrl(download_path, headers) -> str:
+    YandexDownloadUrl = "https://cloud-api.yandex.net/v1/disk/resources/upload"
     request = requests.get(YandexDownloadUrl, headers=headers, params={'path' : download_path})
     if request.status_code != 200 :
         print("something goes wrong : " + str(request.status_code))
         return ""
     return request.json()['href']
+
 def PutFile(url, headers, file_path) :
     request = requests.put(url, data=open(file_path, 'rb'), headers=headers)
     if request.status_code != 201 and request.status_code != 202 :
         print("something goes wrong : " + str(request.status_code))
         return False
     return True
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         usage="%(prog)s [-u] FILE...",
@@ -29,8 +29,13 @@ if __name__ == "__main__":
     parser.add_argument('-upload', '--upload', '-u', type=str, nargs=1)
     file_path = parser.parse_args().upload[0]
     filename = file_path[file_path.rfind('/') + 1 :]
+    # assume that we have config.json in our root
+    config = json.load(open('config.json'))
+    mime = magic.Magic(mime=True)
+    Content_type = mime.from_file(file_path)
+    headers = {'Accept' : 'application/json', 'Authorization' : config['OAuth']}
     headers_send = {'Content-type' : Content_type, 'Slug' : filename}
-    download_url = GetDownloadUrl("/test/Student1ID.jpeg", headers)
+    download_url = GetDownloadUrl("/test/Student2ID.jpeg", headers)
     if download_url == "" :
         exit()
     PutFile(download_url, headers_send, file_path)
