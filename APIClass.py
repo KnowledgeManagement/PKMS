913124019bd78b41f6be9a8561c15941eb196ac6
Petr <ppritup2002@mail.ru>
2022-07-17 21:45:04 +0300

    feat(gh-2): extension of upload function

--- APIClass.py
+++ APIClass.py
@@ -26,7 +26,7 @@ class API:
 
     def GetDownloadUrl(self, path, headers) -> str:
         request = requests.get(self.key_values["YandexDownloadUrl"], headers=headers, params={'path' : path})
-        if request.status_code != 200 :
+        if request.status_code != 200:
             print("something goes wrong : " + str(request.status_code))
             return ""
         return request.json()['href']
@@ -55,11 +55,11 @@ class API:
         list_of_dir = []
         list_of_files = []
         request = requests.get(self.key_values["YandexDataUrl"], headers=headers, params={'path' : path, 'sort' : 'path'})
-        if request.status_code != 200 :
+        if request.status_code != 200:
             print("something goes wrong : " + str(request.status_code))
             print(request.json()['description'])
             exit()
-        for item in request.json()["_embedded"]['items'] :
+        for item in request.json()["_embedded"]['items']:
             if item["type"] == 'dir':
                 list_of_dir.append(item["name"])
             if item['type'] == 'file':
@@ -70,29 +70,69 @@ class API:
             print("f", i, list_of_files[i][0], DecimalSize(list_of_files[i][1]), list_of_files[i][2])
         return [list_of_dir, list_of_files]
 
+    def CheckPathExistance(self, path) -> bool:
+        headers = {'Accept' : 'application/json', 'Authorization' : self.config['OAuth']}
+        request = requests.get(self.key_values["YandexDataUrl"], headers=headers, params={'path' : path, 'sort' : 'path'})
+        if request.status_code != 200:
+            return False
+        return True
+
     def GetUploadUrl(self, download_path, headers) -> str:
         request = requests.get(self.key_values["YandexUploadUrl"], headers=headers, params={'path' : download_path})
         if request.status_code != 200 :
-            print("something goes wrong : " + str(request.status_code))
+            description = request.json()["description"]
+            print(f"something goes wrong : {description}" + str(request.status_code))
             return ""
         return request.json()['href']
 
-    def PutFile(self, url, headers, file_path) :
+    def PutFile(self, url, headers, file_path):
         request = requests.put(url, data=open(file_path, 'rb'), headers=headers)
-        if request.status_code != 201 and request.status_code != 202 :
-            print("something goes wrong : " + str(request.status_code))
+        if request.status_code != 201 and request.status_code != 202:
+            description = request.json()["description"]
+            print(f"something goes wrong : {description}" + str(request.status_code))
             return False
         return True
+
+    def ChoosePath(self) -> str:
+        headers = {'Accept' : 'application/json', 'Authorization' : self.config['OAuth']}
+        path = '/'
+        list_of_data = self.GetMetaData(path, headers=headers)
+        req = input()
+        while req != "exit":
+            if "cd " in req:
+                trash, number =  req.split()
+                if number == ".." or number == '../':
+                    path = str(Path(path).parent)
+                    list_of_data = self.GetMetaData(path, headers=headers)
+                    req = input()
+                    continue
+                if int(number) >= len(list_of_data[0]):
+                    print("Wrong dir number")
+                    req = input()
+                    continue
+                path += list_of_data[0][int(number)]
+                path += '/'
+                list_of_data = self.GetMetaData(path, headers=headers)
+                req = input()
+                continue
+            elif "here":
+                return path
+            else:
+                print("Wrong command")
+                req = input()
+        return path
 
     def UploadScript(self, file_path, file_name, disk_path):
         mime = magic.Magic(mime=True)
         Content_type = mime.from_file(str(file_path))
         headers = {'Accept' : 'application/json', 'Authorization' : self.config['OAuth']}
         headers_send = {'Content-type' : Content_type, 'Slug' : file_name}
-        download_url = self.GetUploadUrl(disk_path + '/' + file_name, headers)
-        if download_url == "" :
+        download_url = self.GetUploadUrl(disk_path + file_name, headers)
+        if download_url == "":
             exit()
+        print(f"[+] uploading file")
         self.PutFile(download_url, headers_send, str(file_path))
+        print(f"[+] file {file_name} successfully uploaded to {file_path}")
 
     def DownloadScript(self):
         headers = {'Accept' : 'application/json', 'Authorization' : self.config['OAuth']}
@@ -102,7 +142,7 @@ class API:
         while req != "exit":
             if "cd" in req:
                 trash, number =  req.split()
-                if number == "..":
+                if number == ".." or number == '../':
                     path = str(Path(path).parent)
                     list_of_data = self.GetMetaData(path, headers=headers)
                     req = input()
@@ -128,6 +168,8 @@ class API:
                 print("[+] Processing...")
                 self.CreateFileFromUrl(download_url, list_of_data[1][int(number)][0])
                 print(f"[+] File {Path(path).name} downloaded successfully")
+                path = str(Path(path).parent)
+                list_of_data = self.GetMetaData(path, headers=headers)
             else:
                 print("Wrong command")
                 req = input()
